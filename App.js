f7a1ba613c1654d9c7319a56c05efcfc8d7d3353
Sumitsrk25 <sumitkalyana25@gmail.com>
2022-10-29 20:18:36 +0530

    fitmorphs1 first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import { Header } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Home  from './src/screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+            <StatusBar style="auto" />
+      <Home />
+
     </View>
   );
 }
@@ -17,4 +21,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
